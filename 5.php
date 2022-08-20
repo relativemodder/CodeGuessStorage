faeea824d65ff115c2e707b035eb352305c6f1ce
Andrey <61388196+relativemodder@users.noreply.github.com>
2022-08-20 17:42:17 +0300

    Create 5.php

---
+++ 5.php
@@ -0,0 +1,5 @@
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+/* Server parameters */
+$cfg['Servers'][$i]['host'] = 'localhost';
+$cfg['Servers'][$i]['compress'] = false;
+$cfg['Servers'][$i]['AllowNoPassword'] = false;
