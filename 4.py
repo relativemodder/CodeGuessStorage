8c997bde4d6fce49b2d45e77e7c79d0ce5ecc970
Andrey <61388196+relativemodder@users.noreply.github.com>
2022-08-20 17:41:26 +0300

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,8 @@
+def prefetch_test(opt):
+  os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
+
+  Dataset = dataset_factory[opt.dataset]
+  opt = opts().update_dataset_info_and_set_heads(opt, Dataset)
+  print(opt)
+  Logger(opt)
+  Detector = detector_factory[opt.task]
